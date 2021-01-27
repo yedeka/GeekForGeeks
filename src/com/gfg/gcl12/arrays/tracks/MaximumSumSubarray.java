c09d61b77677f24ee9ffa0a926b8d8b1c6d499c0
yedekar3 <Yogi#2606>
2021-01-27 04:58:00 -0700

    Logic to find maximum sum subarray.

---
+++ src/com/gfg/gcl12/arrays/tracks/MaximumSumSubarray.java
@@ -0,0 +1,18 @@
+package com.gfg.gcl12.arrays.tracks;
+
+public class MaximumSumSubarray {
+    private static  int findMaxSubArraySum(int[] input){
+        int maxEnd = input[0], result = input[0];
+        for(int i=1;i<input.length;i++){
+            // The idea is to compare the sum until current element to see if the element is bigger than current sum. If yes then we choose element else we extend the previous window.
+            maxEnd = Math.max(maxEnd+input[i], input[i]);
+            result = Math.max(maxEnd, result);
+        }
+        return result;
+    }
+    public static void main(String[] args){
+        int[] input = new int[]{-3,8,-2,4,-5,6};
+        int maxSum = findMaxSubArraySum(input);
+        System.out.println("Maximum sum of any subarray => "+maxSum);
+    }
+}
